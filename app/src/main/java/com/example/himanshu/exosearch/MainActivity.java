c7f10dff16c09a92fcfbbddd72d91e575e9137f7
Divyanshuk <divyanshuk34@gmail.com>
2018-12-13 11:37:48 +0530

    yahoo

--- app/src/main/java/com/example/himanshu/exosearch/MainActivity.java
+++ app/src/main/java/com/example/himanshu/exosearch/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.himanshu.exosearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.GridView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-GridView gridPlanet;
-ArrayList<Integer> arrayList;
+    GridView gridPlanet;
+    ArrayList<Integer> arrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
